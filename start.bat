cdfefd67241011ceb216903ef6254dba14e0ffa8
XternA <XternA@users.noreply.github.com>
2024-06-11 01:07:00 +0100

    Windows Bootstrap Script Improvement and Fixes (#15)
    * Fixed and improved bootstrap script to work better and previously failing if shell is default bash
    
    * Remove git check as not required

--- start.bat
+++ start.bat
@@ -9,21 +9,41 @@ if %errorlevel% neq 0 (
 
 wsl --version >nul 2>&1
 if %errorlevel% equ 0 (
-    wsl [ -d ~/.income-generator ] && wsl "${SHELL##*/}" -ilc "igm" || (
-        echo No Income Generator Tool found. Fetching...
-        echo:
-
-        wsl git clone --depth=1 https://github.com/XternA/income-generator.git ~/.income-generator
-        wsl grep -q "igm='(cd ~/.income-generator; sh start.sh)'" ~/."${SHELL##*/}"rc || (
-            wsl echo "alias igm='(cd ~/.income-generator; sh start.sh)'" >> ~/."${SHELL##*/}"rc
-            wsl source ~/."${SHELL##*/}"rc
-        )
-
-        echo:
-        echo Launching...
-        timeout /t 2 /nobreak >nul 2>&1
-        wsl "${SHELL##*/}" -ilc "igm"
-    )
+    wsl ! [ -d $HOME/.income-generator ] && call :GetTool
+    call :CheckAndRegisterAlias
+    wsl ${SHELL##*/} -ilc "igm"
 ) else (
     echo No Windows Subsystem for Linux found on the system. Ensure WSL is enabled before proceeding.
 )
+exit /b 0
+
+
+REM -- Sub-calls -------------
+:CheckAndRegisterAlias
+wsl -e sh -c "if [ -f $HOME/.aliases ]; then exit 0; else exit 1; fi"
+if %errorlevel% equ 0 (
+    wsl grep -q "igm='(cd $HOME/.income-generator; sh start.sh)'" "$HOME/.aliases" || call :RegisterToAliases
+) else (
+    wsl grep -q "igm='(cd $HOME/.income-generator; sh start.sh)'" "$HOME/.${SHELL##*/}rc" || call :RegisterToRC
+)
+exit /b 0
+
+:RegisterToAliases
+wsl -e sh -c "echo \"alias igm='(cd $HOME/.income-generator; sh start.sh)'\" >> $HOME/.aliases"
+exit /b 0
+
+:RegisterToRC
+wsl -e sh -c "echo \"alias igm='(cd $HOME/.income-generator; sh start.sh)'\" >> $HOME/.${SHELL##*/}rc"
+exit /b 0
+
+:GetTool
+echo:
+echo No Income Generator found. Fetching...
+echo:
+
+wsl git clone --depth=1 https://github.com/XternA/income-generator.git $HOME/.income-generator
+
+echo:
+echo Launching...
+timeout /t 2 /nobreak >nul 2>&1
+exit /b 0
