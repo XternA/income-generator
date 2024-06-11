9698cb9fd7c3038d9f22e283872663e5cec782c1
XternA <xternia@gmail.com>
2024-06-11 00:06:03 +0000

    Remove git check as not required

--- start.bat
+++ start.bat
@@ -7,12 +7,6 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-git --version >nul 2>&1
-if %errorlevel% neq 0 (
-    echo No git found on the system. Please install git before proceeding.
-    exit /b 1
-)
-
 wsl --version >nul 2>&1
 if %errorlevel% equ 0 (
     wsl ! [ -d $HOME/.income-generator ] && call :GetTool
@@ -24,22 +18,22 @@ if %errorlevel% equ 0 (
 exit /b 0
 
 
-REM -- FUNCTIONS ---------
+REM -- Sub-calls -------------
 :CheckAndRegisterAlias
 wsl -e sh -c "if [ -f $HOME/.aliases ]; then exit 0; else exit 1; fi"
 if %errorlevel% equ 0 (
-    wsl grep -q "igm='(cd ~/.income-generator; sh start.sh)'" "$HOME/.aliases" || call :RegisterToAliases
+    wsl grep -q "igm='(cd $HOME/.income-generator; sh start.sh)'" "$HOME/.aliases" || call :RegisterToAliases
 ) else (
-    wsl grep -q "igm='(cd ~/.income-generator; sh start.sh)'" "$HOME/.${SHELL##*/}rc" || call :RegisterToRC
+    wsl grep -q "igm='(cd $HOME/.income-generator; sh start.sh)'" "$HOME/.${SHELL##*/}rc" || call :RegisterToRC
 )
 exit /b 0
 
 :RegisterToAliases
-wsl -e sh -c "echo \"alias igm='(cd ~/.income-generator; sh start.sh)'\" >> $HOME/.aliases"
+wsl -e sh -c "echo \"alias igm='(cd $HOME/.income-generator; sh start.sh)'\" >> $HOME/.aliases"
 exit /b 0
 
 :RegisterToRC
-wsl -e sh -c "echo \"alias igm='(cd ~/.income-generator; sh start.sh)'\" >> $HOME/.${SHELL##*/}rc"
+wsl -e sh -c "echo \"alias igm='(cd $HOME/.income-generator; sh start.sh)'\" >> $HOME/.${SHELL##*/}rc"
 exit /b 0
 
 :GetTool
