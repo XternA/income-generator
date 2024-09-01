73402ff34bf9c80aafb28c32d78726d852658f48
XternA <XternA@users.noreply.github.com>
2024-09-01 02:01:56 +0100

    Update shared component

--- scripts/shared-component.sh
+++ scripts/shared-component.sh
@@ -7,15 +7,16 @@ export YELLOW='\033[1;93m'
 export PINK='\033[1;95m'
 export NC='\033[0m'
 
-export ENV_FILE="$(pwd)/.env"
-export ENV_DEPLOY_FILE="$(pwd)/.env.deploy"
-export JSON_FILE="$(pwd)/apps.json"
+export ENV_FILE=".env"
+export ENV_SYSTEM_FILE=".env.system"
+export ENV_DEPLOY_FILE=".env.deploy"
+export JSON_FILE="apps.json"
 
 # Declared util component ----------------
-ARCH="sh scripts/arch.sh"
-CLEANUP="sh scripts/cleanup.sh"
-APP_SELECTION="sh scripts/app-selection.sh"
-APP_CONFIG="sh scripts/config.sh"
-BACKUP_RESTORE="sh scripts/backup-restore.sh"
-SET_LIMIT="sh scripts/set-limit.sh"
-UPDATE_CHECKER="sh scripts/check-tool-update.sh"
+export ARCH="sh scripts/arch.sh"
+export CLEANUP="sh scripts/cleanup.sh"
+export APP_SELECTION="sh scripts/app-selection.sh"
+export APP_CONFIG="sh scripts/config.sh"
+export BACKUP_RESTORE="sh scripts/backup-restore.sh"
+export SET_LIMIT="sh scripts/set-limit.sh"
+export UPDATE_CHECKER="sh scripts/check-tool-update.sh"
