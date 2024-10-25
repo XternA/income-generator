6145d4ed1032131306f2b3cf473ec62947ddf998
XternA <XternA@users.noreply.github.com>
2024-10-25 09:59:44 +0100

    Optimise check for update

--- scripts/check-tool-update.sh
+++ scripts/check-tool-update.sh
@@ -9,10 +9,10 @@ case "$1" in
         URL="https://api.github.com/repos"
         REPO="xterna/income-generator"
 
-        RELEASE_VERSION=$(curl -s "$URL/$REPO/releases/latest" | jq -r '.tag_name // empty'); [ -z "$RELEASE_VERSION" ] && exit 0
-        TAG_COMMIT=$(curl -s "$URL/$REPO/tags" | jq -r --arg tag "$RELEASE_VERSION" '.[] | select(.name == $tag) | .commit.sha'); [ -z "$TAG_COMMIT" ] && exit 0
+        RELEASE_VERSION=$(curl -s --connect-timeout 2 --max-time 5 "$URL/$REPO/releases/latest" | jq -r '.tag_name'); [ -z "$RELEASE_VERSION" ] && exit 0
+        TAG_COMMIT=$(curl -s "$URL/$REPO/tags" | jq -r --arg tag "$RELEASE_VERSION" '.[] | select(.name == $tag) | .commit.sha' | head -n 1); [ -z "$TAG_COMMIT" ] && exit 0
         LOCAL_COMMIT=$(git rev-parse HEAD)
-        COMPARE_COMMITS=$(curl -s "$URL/$REPO/compare/$TAG_COMMIT...$LOCAL_COMMIT" | jq -r '.status // empty')
+        COMPARE_COMMITS=$(curl -s --connect-timeout 2 --max-time 5 "$URL/$REPO/compare/$TAG_COMMIT...$LOCAL_COMMIT" | jq -r '.status')
 
         [ "$COMPARE_COMMITS" = "behind" ] && printf "\033[5m\033[91m%s\033[0m\n" "New tool update available! 🚀"
         ;;
