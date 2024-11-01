37b1dd60bd240655c00b296bdff2217b5023495d
XternA <XternA@users.noreply.github.com>
2024-11-01 05:19:25 +0000

    Proxy Feature - Enhancement (#48)
    * Optimise UUID generating logic
    
    * Add script for generating UUIDs for app file
    
    * Update generate UUID file logic to add no more than proxy file
    
    * Add ability to view UUIDs for proxy apps that require multiple ID registrations
    
    * Update UUID generator script with abiltiy to get UUID file
    
    * Fix incorrect logic for UUID generating
    
    * Moved souring of external file
    
    * Update viewing proxy UUID logic
    
    * Add option to view all generated UUIDs
    
    * Update .gitignore
    
    * Export proxy folder variable
    
    * Update display visual
    
    * Update view proxy UUID logic
    
    * Update application JSON definition
    
    * Update an enhanced proxy menu with extra features
    
    * Update proxy manager with ability to now install applications that require unique UUID
    
    * Update information description
    
    * Update menu wording
    
    * Update help documentation

--- scripts/proxy/proxy-menu.sh
+++ scripts/proxy/proxy-menu.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. "scripts/proxy/proxy-uuid-generator.sh"
+
 HAS_PROXY_APPS="$CONTAINER_ALIAS ps -a -q -f 'label=project=proxy' | head -n 1"
 
 display_banner() {
@@ -46,6 +48,135 @@ install_proxy_app() {
 
 remove_proxy_app() {
     sh "scripts/proxy/proxy-manager.sh" remove
+}
+
+edit_proxy_file() {
+    while true; do
+        display_banner
+
+        if [ ! -z $(eval "$HAS_PROXY_APPS") ]; then
+            echo "Proxy application still active."
+            echo "\nRemove existing applications first."
+            printf "\nPress Enter to continue..."; read input
+            return
+        fi
+        if [ ! -d "$PROXY_FOLDER" ]; then
+            echo "No multi-UUIDs application entries found."
+            printf "\nPress Enter to continue..."; read input
+            return
+        fi
+
+        uuid_files="$(ls -1v "$PROXY_FOLDER" | sed 's/\.[^.]*$//')"
+        total_files="$(echo "$uuid_files" | wc -l)"
+        options="(1-${total_files})"
+
+        echo "Current applications with multiple UUIDs.\n"
+
+        printf "%-4s %-21s\n" "No." "Name"
+        printf "%-4s %-21s\n" "---" "--------------------"
+        printf "%s\n" "$uuid_files" | awk -v GREEN="$GREEN" -v NC="$NC" '
+        BEGIN { counter = 1 }
+        {
+            printf "%-4s %s%-21s%s\n", counter, GREEN, $1, NC
+            counter++
+        }'
+        echo "\nOption:\n  ${YELLOW}0${NC} = ${YELLOW}exit${NC}"
+
+        printf "\nSelect an entry to edit $options: "
+        read -r input
+
+        case "$input" in
+            ''|*[!0-9]*)
+                echo "\nInvalid option. Please select a valid option $options."
+                printf "\nPress Enter to continue..."; read input
+                continue
+                ;;
+        esac
+
+        [ "$input" -eq 0 ] && break
+
+        if [ "$input" -ge 1 ] && [ "$input" -le "$total_files" ]; then
+            display_banner
+            app="$(set -- $uuid_files; eval echo \${$input})"
+            echo "After making changes, press '${BLUE}CTRL + X${NC}' and press '${BLUE}Y${NC}' to save changes."
+            printf "\nPress Enter to continue..."; read input
+            nano "${PROXY_FOLDER}/${app}.uuid"
+        else
+            echo "\nInvalid option. Please select a valid option $options."
+            printf "\nPress Enter to continue..."; read input
+        fi
+    done
+}
+
+manage_uuids() {
+    while true; do
+        display_banner
+
+        options="(1-3)"
+        echo "1. View all generated UUID"
+        echo "2. Edit generated UUIDs"
+        echo "3. Clear generated UUIDs"
+        echo "0. Return to Main Menu"
+        echo
+        read -p "Select an option $options: " choice
+
+        case $choice in
+            0) break ;;
+            1)
+                display_banner
+                if [ ! -d "$PROXY_FOLDER" ]; then
+                    echo "No multi-UUIDs application entries found.\n"
+                else
+                    view_proxy_uuids all
+                fi
+                printf "Press Enter to continue..."; read input
+                ;;
+            2) edit_proxy_file ;;
+            3)
+                while true; do
+                    display_banner
+
+                    if [ ! -z $(eval "$HAS_PROXY_APPS") ]; then
+                        echo "Proxy application still active."
+                        echo "\nRemove existing applications first."
+                        printf "\nPress Enter to continue..."; read input
+                        break
+                    fi
+                    if [ ! -d "$PROXY_FOLDER" ]; then
+                        echo "No multi-UUIDs application entries found."
+                        printf "\nPress Enter to continue..."; read input
+                        return
+                    fi
+
+                    echo "Do you want to clear all application generated multi-UUIDs?\n"
+                    echo "You might want to backup if you wish to re-use the IDs later.\n"
+                    read -p "Do you really want to delete all entries? (Y/N): " yn
+
+                    case $yn in
+                        [Yy]*)
+                            display_banner
+                            rm -rf "$PROXY_FOLDER"
+                            echo "All application generated UUIDs have been removed."
+                            printf "\nPress Enter to continue..."; read input
+                            break
+                            ;;
+                        [Nn]*)
+                            break
+                            ;;
+                        *)
+                            display_banner
+                            echo "Please enter yes (Y/y) or no (N/n)."
+                            printf "\nPress Enter to continue..."; read input
+                            ;;
+                    esac
+                done
+                ;;
+            *)
+                echo "\nInvalid option. Please select a valid option $options."
+                printf "\nPress Enter to continue..."; read input
+                ;;
+        esac
+    done
 }
 
 view_proxy() {
@@ -99,6 +230,24 @@ reset_proxy() {
     fi
 }
 
+view_uuids() {
+    display_banner
+
+    if [ -d "$PROXY_FOLDER" ]; then
+        echo "Multi-UUID applications with instruction need to be registered."
+        echo "Unregisted IDs will not count towards earnings.\n"
+
+        echo "Deployed applications with in-use unqiue UUIDs are shown here.\n"
+
+        view_proxy_uuids active
+        printf "Press Enter to continue..."; read input
+    else
+        echo "Application with multi-UUIDs are auto generated during installtion.\n"
+        echo "After installation, check here to view UUIDs and further instructions."
+        printf "\nPress Enter to continue..."; read input
+    fi
+}
+
 main_menu() {
     while true; do
         display_banner
@@ -106,14 +255,16 @@ main_menu() {
         TOTAL_PROXIES=$([ -e "$PROXY_FILE" ] && awk 'NF {count++} END {print count}' "$PROXY_FILE" || echo 0)
         echo "Available Proxies: ${RED}${TOTAL_PROXIES}${NC}\n"
 
-        options="(1-7)"
+        options="(1-8)"
         echo "1. Setup Proxies"
         echo "2. Select Applications"
         echo "3. Install Proxy Applications"
         echo "4. Remove Proxy Applications"
         echo "5. Show Installed Applications"
-        echo "6. View Proxies"
-        echo "7. Reset Proxies"
+        echo "6. View Active UUIDs"
+        echo "7. Manage UUIDs"
+        echo "8. View Proxies"
+        echo "9. Reset Proxies"
         echo "0. Quit"
         echo
         read -p "Select an option $options: " choice
@@ -125,8 +276,10 @@ main_menu() {
             3) install_proxy_app ;;
             4) remove_proxy_app ;;
             5) show_applications proxy ;;
-            6) view_proxy ;;
-            7) reset_proxy ;;
+            6) view_uuids ;;
+            7) manage_uuids ;;
+            8) view_proxy ;;
+            9) reset_proxy ;;
             *)
                 echo "\nInvalid option. Please select a valid option $options."
                 printf "\nPress Enter to continue..."; read input
@@ -148,5 +301,6 @@ case "$1" in
     install) install_proxy_app ;;
     remove) remove_proxy_app ;;
     reset) reset_proxy ;;
+    id) view_uuids ;;
     *) main_menu ;;
 esac
---
+++ scripts/proxy/proxy-uuid-generator.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+. "${ROOT_DIR}/scripts/util/uuid-generator.sh"
+
+export PROXY_FOLDER="${ROOT_DIR}/proxy_uuid"
+TOTAL_PROXIES="$(awk 'BEGIN {count=0} NF {count++} END {print count}' "$PROXY_FILE")"
+
+generate_uuid_files() {
+    app_data="jq -r '.[] | select(.is_enabled == true and .proxy_uuid != null) | \"\(.name) \(.proxy_uuid)\"' \"$JSON_FILE\""
+
+    [ -n "$PROXY_FOLDER" ] && mkdir -p "$PROXY_FOLDER"
+
+    counter=1
+    while true; do
+        echo "$(eval $app_data)" | while read -r name proxy_uuid; do
+            requires_uuid=$(echo "$proxy_uuid" | jq -r '.requires_uuid')
+            uuid_type=$(echo "$proxy_uuid" | jq -r '.uuid_type')
+
+            proxy_file="${PROXY_FOLDER}/${name}.uuid"
+            uuid="$(generate_uuid "$uuid_type")"
+
+            if [ -f "$proxy_file" ]; then
+                current_uuid_count="$(awk 'BEGIN {count=0} NF {count++} END {print count}' "$proxy_file")"
+                [ "$current_uuid_count" -lt "$TOTAL_PROXIES" ] && printf "${uuid}\n" >> "$proxy_file"
+            else
+                printf "${uuid}\n" > "$proxy_file"
+            fi
+        done
+
+        [ "$counter" -ge "$TOTAL_PROXIES" ] && break || counter=$((counter + 1))
+    done
+}
+
+view_proxy_uuids() {
+    if [ "$1" = "all" ]; then
+        for file in "$PROXY_FOLDER"/*; do
+            [ -f "$file" ] || continue
+
+            filename="${file##*/}"
+            app_name="${filename%%.*}" # Remove extension
+
+            echo "[ ${RED}${app_name}${NC} ]${NC}"
+            counter=0
+            while IFS= read -r line; do
+                [ $(( counter % 2 )) -eq 0 ] && echo "${YELLOW}$line" || echo "${BLUE}$line"
+                counter=$((counter + 1))
+            done < "$file"
+            echo "$NC"
+        done
+        return
+    fi
+
+    echo "Active Proxies: ${RED}${TOTAL_PROXIES}${NC}\n"
+
+    if [ "$1" = "active" ]; then
+        app_data=$(jq -r '.[] | select(.is_enabled == true and .proxy_uuid != null) | "\(.name) \(.proxy_uuid.description)"' "$JSON_FILE")
+
+        [ -z "$app_data" ] && echo "No active application with multi-UUID currently in use.\n" && return
+
+        echo "$app_data" | while read -r name description; do
+            file="${PROXY_FOLDER}/${name}.uuid"
+
+            [ -f "$file" ] || continue
+
+            echo "[ ${GREEN}${name}${NC} ]${NC}"
+            [ "$description" != null ] && echo "${PINK}$description${NC}\n"
+
+            counter=0
+            while IFS= read -r line; do
+                [ $counter -lt $TOTAL_PROXIES ] || continue # List in-use ID's corresponding to proxy count
+                [ $(( counter % 2 )) -eq 0 ] && echo " -> ${YELLOW}$line" || echo " -> ${BLUE}$line"
+                counter=$((counter + 1))
+            done < "$file"
+            echo "$NC"
+        done
+        return
+    fi
+
+    for file in "$PROXY_FOLDER"/*; do
+        [ -f "$file" ] || continue
+
+        filename="${file##*/}"
+        app_name="${filename%%.*}" # Remove extension
+        description=$(jq -r --arg name "${app_name}" '.[] | select(.name == $name and .proxy_uuid != null) | .proxy_uuid.description' "$JSON_FILE")
+
+        echo "[ ${GREEN}${app_name}${NC} ]${NC}"
+        [ "$description" != null ] && echo "${PINK}$description${NC}\n"
+
+        counter=0
+        while IFS= read -r line; do
+            [ $counter -lt $TOTAL_PROXIES ] || continue # List in-use ID's corresponding to proxy count
+            [ $(( counter % 2 )) -eq 0 ] && echo " -> ${YELLOW}$line" || echo " -> ${BLUE}$line"
+            counter=$((counter + 1))
+        done < "$file"
+        echo "$NC"
+    done
+}
+
+get_proxy_file() {
+    local app_name="$1"
+    [ -f "${PROXY_FOLDER}/${app_name}.uuid" ] && echo "${PROXY_FOLDER}/${app_name}.uuid"
+}
