90d6559ae034cc7e5b1ef507353ae3a0bf68bc75
XternA <XternA@users.noreply.github.com>
2025-09-19 02:48:04 +0100

    Improve app limit with guards and prevention of defining over limit

--- scripts/proxy/proxy-app-limiter.sh
+++ scripts/proxy/proxy-app-limiter.sh
@@ -46,6 +46,8 @@ proxy_app_limiter() {
 
         selected_app=$(printf '%s\n' "$limit_data" | awk -v n="$choice" 'NR==n {print $1}')
         current_value=$(awk -F= -v app="$selected_app" '$1 == app {print $2}' "$PROXY_INSTALL_LIMIT")
+        default_value=$(extract_app_data_fields_only "$selected_app" .install_limit)
+
         if [ "$current_value" = "-" ]; then
             display_current_value="Unlimited"
         else
@@ -57,6 +59,9 @@ proxy_app_limiter() {
             printf "Configuring proxy install count limit.\n\n"
             printf "Application:   ${GREEN}%s${NC}\n" "$selected_app"
             printf "Current Limit: ${YELLOW}%s${NC}\n" "$display_current_value"
+            if [ "$default_value" != "null" ]; then
+                printf "Maximum Limit: ${PINK}%s${NC}\n" "$default_value"
+            fi
             printf "\nOptions:\n  ${RED}0${NC} = ${RED}reset to default${NC}\n"
             printf "\nPress Enter to keep current setting.\n"
             printf "\nEnter new limit: "; read -r new_value
@@ -69,7 +74,6 @@ proxy_app_limiter() {
 
             # Reset to default
             if [ "$new_value" = "0" ]; then
-                default_value=$(extract_app_data_fields_only "$selected_app" .install_limit)
                 if [ "$default_value" = "null" ]; then
                     new_value="0"
                 else
@@ -78,14 +82,21 @@ proxy_app_limiter() {
             fi
 
             case "$new_value" in
-                *[!0-9]*)
-                    printf "\nInvalid input. Enter limit in number quantity only.\n"
+                ''|*[!0-9]*)
+                    printf "\nInvalid input. Enter a whole number only.\n"
                     printf "\nPress Enter to continue..."; read -r _
                     continue
                     ;;
                 ''|0*)
                     new_value="-"
                     break
+                    ;;
+                *)
+                    if [ "$new_value" -gt "$default_value" ]; then
+                        printf "\nLimit set cannot exceed ${GREEN}$default_value${NC}.\n"
+                        printf "\nPress Enter to continue..."; read -r _
+                        continue
+                    fi
                     ;;
             esac
 
