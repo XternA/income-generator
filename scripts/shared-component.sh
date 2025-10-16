2e67d2f6971201f11506bebb6f721b3d314b92f5
XternA <XternA@users.noreply.github.com>
2025-10-16 23:45:31 +0100

    Update IGM with true colour and fallback

--- scripts/shared-component.sh
+++ scripts/shared-component.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-export RED='\033[1;91m'
-export GREEN='\033[1;92m'
-export BLUE='\033[1;96m'
-export YELLOW='\033[1;93m'
-export PINK='\033[1;95m'
+if [ "$COLORTERM" = "truecolor" ] || [ "$COLORTERM" = "24bit" ]; then
+    export RED='\033[1;38;2;255;105;135m'
+    export GREEN='\033[1;38;2;120;255;180m'
+    export BLUE='\033[1;38;2;150;240;255m'
+    export YELLOW='\033[1;38;2;255;190;80m'
+    export PINK='\033[1;38;2;255;121;198m'
+    export GREY='\033[38;2;50;50;50m'
+else
+    export RED='\033[1;91m'
+    export GREEN='\033[1;92m'
+    export BLUE='\033[1;96m'
+    export YELLOW='\033[1;93m'
+    export PINK='\033[1;95m'
+    export GREY='\033[90m'
+fi
 export NC='\033[0m'
 
+# Shared path files
 export ROOT_DIR=$(pwd)
 export ENV_FILE="$ROOT_DIR/.env"
 export ENV_SYSTEM_FILE="$ROOT_DIR/.env.system"
